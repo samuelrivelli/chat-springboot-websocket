a685c56560aef7414144ed19fa5f95e847be27ee
Samuel Rivelli <samuelmagrivelli@gmail.com>
2024-05-11 13:58:46 -0300

    Implementações iniciais

---
+++ src/main/java/com/example/chat/chat/chatController.java
@@ -0,0 +1,24 @@
+package com.example.chat.chat;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class chatController {
+
+    @MessageMapping("/chat.sendMessage")
+    @SendTo("/topic/public")
+    public ChatMessage sendMessage(@Payload ChatMessage chatMessage){
+        return chatMessage;
+    }
+    //teste
+    @MessageMapping("/chat.addUser")
+    @SendTo("/topic/public")
+    public ChatMessage addUser(@Payload ChatMessage chatMessage, SimpMessageHeaderAccessor headerAcessor){
+        headerAcessor.getSessionAttributes().put("username", chatMessage.getSender());
+        return chatMessage;
+    }
+}
