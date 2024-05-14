1cc832d5255f12df8f4256a734d77f7161eaa96c
Samuel Rivelli <samuelmagrivelli@gmail.com>
2024-05-13 23:46:12 -0300

    Correçao  de erro

--- src/main/java/com/example/chat/config/SecurityConfig.java
+++ src/main/java/com/example/chat/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package dev.danvega.social.config;
+package com.example.chat.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
